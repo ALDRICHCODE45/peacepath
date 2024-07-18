934666622b3e78e4b7061c569988ae1bbfe29905
ALDRICH_CODE45 <nocheblanca92@gmail.com>
2024-07-17 18:02:28 -0600

    production_url_redirection_addeD

--- src/actions/uploadAudioToAwsS3.ts
+++ src/actions/uploadAudioToAwsS3.ts
@@ -6,16 +6,16 @@ import {
 } from "@aws-sdk/client-s3";
 
 const s3 = new S3Client({
-  region: process.env.NEXT_PUBLIC_AWS_S3_REGION ?? "",
+  region: process.env.AWS_S3_REGION ?? "",
   credentials: {
-    accessKeyId: process.env.NEXT_PUBLIC_AWS_S3_ACCESS_KEY ?? "",
-    secretAccessKey: process.env.NEXT_PUBLIC_AWS_S3_SECRET_KEY ?? "",
+    accessKeyId: process.env.AWS_S3_ACCESS_KEY ?? "",
+    secretAccessKey: process.env.AWS_S3_SECRET_KEY ?? "",
   },
 });
 
 export const uploadFileToS3 = async (audioBuffer: Buffer, fileName: string) => {
   const params: PutObjectCommandInput = {
-    Bucket: process.env.NEXT_PUBLIC_AWS_S3_BUCKET_NAME!,
+    Bucket: process.env.AWS_S3_BUCKET_NAME!,
     Key: `myfolder/${fileName}`,
     Body: audioBuffer,
     ContentType: "audio/mpeg",
@@ -24,7 +24,7 @@ export const uploadFileToS3 = async (audioBuffer: Buffer, fileName: string) => {
   try {
     const data = await s3.send(command);
     console.log("Successfully uploaded audio file:", data);
-    return data.ServerSideEncryption;
+    return data;
   } catch (err) {
     console.error("Error uploading audio file:", err);
     throw err;
--- src/app/api/auth/creation/route.ts
+++ src/app/api/auth/creation/route.ts
@@ -29,5 +29,5 @@ export async function GET() {
       },
     });
   }
-  return NextResponse.redirect("http://localhost:3000/dashboard");
+  return NextResponse.redirect("https://peacepath.vercel.app/dashboard");
 }
