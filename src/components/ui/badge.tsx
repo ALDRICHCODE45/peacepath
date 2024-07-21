87bd34185bc0889a03c6d0c375de186ad7b245c7
ALDRICH_CODE45 <nocheblanca92@gmail.com>
2024-07-20 18:04:42 -0600

    online baged modified

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -15,7 +15,7 @@ const badgeVariants = cva(
         destructive:
           "border-transparent bg-destructive text-destructive-foreground hover:bg-destructive/80",
         outline: "text-foreground",
-        success: "border-transparent bg-[#587434] text-white ",
+        success: "border-transparent bg-[#cbfd96] text-white ",
       },
     },
     defaultVariants: {
