21e86057073e06f753302d84527051708f9a344d
ALDRICH_CODE45 <nocheblanca92@gmail.com>
2024-07-21 00:07:27 -0600

    loading added

---
+++ src/app/dashboard/talking-zone/loading.tsx
@@ -0,0 +1,17 @@
+import { Skeleton } from "@/components/ui/skeleton";
+import { type ReactElement } from "react";
+
+export interface LoadingProps {}
+
+export default function Loading({}: LoadingProps): ReactElement {
+  return (
+    <div className="max-w-[1000px] mx-auto flex gap-x-10 mt-4 mb-10 ">
+      <div className="w-[65%] flex flex-col gap-y-5">
+        <Skeleton className="w-full h-[1000px]" />
+      </div>
+      <div className="w-[35%] ">
+        <Skeleton className="w-full h-[300px]" />
+      </div>
+    </div>
+  );
+}
