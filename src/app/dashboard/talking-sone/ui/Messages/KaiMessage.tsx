dde22d678e9356c715206c70b8db6f02d990100e
ALDRICH_CODE45 <nocheblanca92@gmail.com>
2024-07-20 20:38:42 -0600

    responsive talking

--- src/app/dashboard/talking-sone/ui/Messages/KaiMessage.tsx
+++ src/app/dashboard/talking-sone/ui/Messages/KaiMessage.tsx
@@ -7,7 +7,7 @@ interface KaiMessageProps {
 export default function KaiMessage({ text }: KaiMessageProps) {
   return (
     <div className="flex items-start gap-4 mb-4 justify-start max-w-full sm:max-w-[800px]">
-      <Avatar className="h-14 w-14">
+      <Avatar className="md:h-14 md:w-14 hidden md:block">
         <AvatarImage src="/koala_profile.jpg" />
         <AvatarFallback>KA</AvatarFallback>
       </Avatar>
--- src/app/dashboard/talking-sone/ui/Messages/UserMessage.tsx
+++ src/app/dashboard/talking-sone/ui/Messages/UserMessage.tsx
@@ -6,11 +6,11 @@ interface UserMessageProps {
 
 export default function UserMessage({ text }: UserMessageProps) {
   return (
-    <div className="flex items-start gap-4 mb-4 justify-end max-w-full sm:max-w-[800px]">
-      <div className="grid gap-2 rounded-lg p-3 text-sm bg-primary text-white">
+    <div className="flex items-start gap-4 mb-4 justify-end max-w-full  sm:max-w-[800px]">
+      <div className="grid gap-2 rounded-lg p-3 text-sm bg-primary text-white max-w-full">
         <div>{text}</div>
       </div>
-      <Avatar className="h-10 w-10">
+      <Avatar className="md:h-10 md:w-10 md:block hidden">
         <AvatarImage src="/placeholder-user.png" />
         <AvatarFallback>US</AvatarFallback>
       </Avatar>
