b7e966aa79447dfc598fa8bf7f90ae8379a21701
ALDRICH_CODE45 <nocheblanca92@gmail.com>
2024-07-20 21:06:10 -0600

    style:header sticky

--- src/components/meditation/Header.tsx
+++ src/components/meditation/Header.tsx
@@ -7,7 +7,7 @@ export interface HeaderProps {}
 
 export function Header({}: HeaderProps): ReactElement {
   return (
-    <header className="border-b dark:border-b-[#363b3d] bg-muted/40 p-4 sm:p-6 bg-white dark:bg-[#181a1b]">
+    <header className="border-b dark:border-b-[#363b3d] bg-muted/40 p-4 sm:p-6 bg-white dark:bg-[#181a1b] sticky ">
       <div className="flex items-center gap-4">
         <Avatar className="h-10 w-10">
           <AvatarImage src="/koala_profile.jpg" />
