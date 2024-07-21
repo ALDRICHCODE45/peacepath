1e232ac05678fc1935977473a777aa333e081618
ALDRICH_CODE45 <nocheblanca92@gmail.com>
2024-07-20 22:11:51 -0600

    online text:

--- src/components/meditation/Header.tsx
+++ src/components/meditation/Header.tsx
@@ -8,21 +8,21 @@ export interface HeaderProps {}
 
 export function Header({}: HeaderProps): ReactElement {
   return (
-    <header className="border-b dark:border-b-[#363b3d] bg-muted/40 p-4 sm:p-6 bg-white dark:bg-[#181a1b] sticky ">
+    <header className="border-b dark:border-b-[#363b3d] bg-muted/40 p-4 sm:p-6 bg-white dark:bg-[#181a1b] sticky">
       <div className="flex h-full items-center justify-between">
-        <div className="flex h-full">
+        <div className="flex items-center gap-4">
           <Avatar className="h-10 w-10">
             <AvatarImage src="/koala_profile.jpg" />
             <AvatarFallback>CB</AvatarFallback>
+          </Avatar>
+          <div className="flex flex-col">
             <p className="font-medium text-black dark:text-white">KAI</p>
             <Badge variant="success">Online</Badge>
-          </Avatar>
-        </div>
-        <div className="flex h-full items-center">
-          <div>
-            <ThemeToggle />
           </div>
-          <div className="justify-end hidden sm:block ml-3">
+        </div>
+        <div className="flex items-center gap-4">
+          <ThemeToggle />
+          <div className="hidden sm:block">
             <UserDropDown />
           </div>
         </div>
