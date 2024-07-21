c4fa85f61f3bb7213ad573acf9ae7fc514824948
ALDRICH_CODE45 <nocheblanca92@gmail.com>
2024-07-20 21:50:14 -0600

    header_sidebar_fixed

--- src/components/meditation/Header.tsx
+++ src/components/meditation/Header.tsx
@@ -9,12 +9,12 @@ export interface HeaderProps {}
 export function Header({}: HeaderProps): ReactElement {
   return (
     <header className="border-b dark:border-b-[#363b3d] bg-muted/40 p-4 sm:p-6 bg-white dark:bg-[#181a1b] sticky ">
-      <div className="flex items-center gap-4">
+      <div className="flex w-full items-center justify-around gap-4">
         <Avatar className="h-10 w-10">
           <AvatarImage src="/koala_profile.jpg" />
           <AvatarFallback>CB</AvatarFallback>
         </Avatar>
-        <div className="flex w-full">
+        <div className="">
           <div>
             <p className="font-medium text-black dark:text-white">KAI</p>
             <Badge variant="success">Online</Badge>
