2a326699b219b0ea6f284893ffe14883bf430377
ALDRICH_CODE45 <nocheblanca92@gmail.com>
2024-07-20 18:07:39 -0600

    online text created

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -15,7 +15,7 @@ const badgeVariants = cva(
         destructive:
           "border-transparent bg-destructive text-destructive-foreground hover:bg-destructive/80",
         outline: "text-foreground",
-        success: "border-transparent bg-[#cbfd96] text-white ",
+        success: "border-transparent bg-[#cbfd96] text-[#2b491f]",
       },
     },
     defaultVariants: {
