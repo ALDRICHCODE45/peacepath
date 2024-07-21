5b6daf8bdac11bbfd6d4d3fbc3071b37c1f20891
ALDRICH_CODE45 <nocheblanca92@gmail.com>
2024-07-20 22:08:11 -0600

    header:flexanimated

--- src/components/meditation/Header.tsx
+++ src/components/meditation/Header.tsx
@@ -10,12 +10,14 @@ export function Header({}: HeaderProps): ReactElement {
   return (
     <header className="border-b dark:border-b-[#363b3d] bg-muted/40 p-4 sm:p-6 bg-white dark:bg-[#181a1b] sticky ">
       <div className="flex h-full items-center justify-between">
-        <Avatar className="h-10 w-10">
-          <AvatarImage src="/koala_profile.jpg" />
-          <AvatarFallback>CB</AvatarFallback>
-          <p className="font-medium text-black dark:text-white">KAI</p>
-          <Badge variant="success">Online</Badge>
-        </Avatar>
+        <div className="flex h-full">
+          <Avatar className="h-10 w-10">
+            <AvatarImage src="/koala_profile.jpg" />
+            <AvatarFallback>CB</AvatarFallback>
+            <p className="font-medium text-black dark:text-white">KAI</p>
+            <Badge variant="success">Online</Badge>
+          </Avatar>
+        </div>
         <div className="flex h-full">
           <ThemeToggle />
           <div className="justify-end hidden sm:block ml-3">
