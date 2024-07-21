97673ab2c5a3125821381c5b16892f62e6efa94b
ALDRICH_CODE45 <nocheblanca92@gmail.com>
2024-07-20 19:04:02 -0600

    text_dark_version added

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -14,7 +14,7 @@ const badgeVariants = cva(
           "border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80",
         destructive:
           "border-transparent bg-destructive text-destructive-foreground hover:bg-destructive/80",
-        outline: "text-foreground",
+        outline: "text-black dark:text-white",
         success: "border-transparent bg-[#cbfd96] text-[#2b491f]",
         success_badged: "border-transparent bg-[#cbfd96] text-black",
       },
