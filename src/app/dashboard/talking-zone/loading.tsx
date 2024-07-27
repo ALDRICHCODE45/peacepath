adb928d80ccc923c088696cdc5a4ff54398fe229
ALDRICH_CODE45 <nocheblanca92@gmail.com>
2024-07-27 13:06:15 -0600

    style:loading changed

--- src/app/dashboard/talking-zone/loading.tsx
+++ src/app/dashboard/talking-zone/loading.tsx
@@ -5,9 +5,9 @@ export interface LoadingProps {}
 
 export default function Loading({}: LoadingProps): ReactElement {
   return (
-    <div className="w-full h-full flex justify-center items-center">
+    <div className="w-full h-full flex justify-center items-center text-9xl">
       <div className="">
-        <Loader2 className="text-black dark:text-white  animate-spin text-5xl" />
+        <Loader2 className="text-black dark:text-white  animate-spin text-9xl" />
       </div>
     </div>
   );
