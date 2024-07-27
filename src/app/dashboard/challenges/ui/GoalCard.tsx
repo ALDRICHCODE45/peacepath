431835af4ba329de182e5ca207bd391828c0b74a
ALDRICH_CODE45 <nocheblanca92@gmail.com>
2024-07-26 23:32:38 -0600

    goal card changed style

--- src/app/dashboard/challenges/ui/GoalCard.tsx
+++ src/app/dashboard/challenges/ui/GoalCard.tsx
@@ -75,8 +75,8 @@ export const GoalCard = ({
   };
 
   return (
-    <Card className="animate__animated animate__fadeInDown relative dark:bg-[#181a1b] rounded-lg p-2 flex flex-col items-center justify-center border dark:border-[#363b3d] w-80 h-60">
-      <div className="w-full flex justify-between items-center ">
+    <Card className="animate__animated animate__fadeInDown relative dark:bg-[#181a1b] rounded-lg p-2 flex flex-col items-center justify-center border dark:border-[#363b3d] w-80 h-60 mb-5">
+      <div className="w-full flex justify-between items-center top-0 mt-0">
         <ShowDetails details={description} />
         <Button
           onClick={handleClick}
