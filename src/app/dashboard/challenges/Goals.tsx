82db1e99789fef2be51ce9614b5ada5cc479c99b
ALDRICH_CODE45 <nocheblanca92@gmail.com>
2024-07-27 17:35:30 -0600

    style: display flex to goals lenght

--- src/app/dashboard/challenges/Goals.tsx
+++ src/app/dashboard/challenges/Goals.tsx
@@ -57,29 +57,35 @@ export default function Goals({ initialGoals }: Props) {
           </h1>
           <DrawerDemo />
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-2  overflow-y-scroll h-[calc(100vh-300px)]">
-          {loading && (
-            <div className="w-full mx-auto flex justify-center">
-              <Loader2 width={50} height={50} className="animate-spin" />
-            </div>
-          )}
-          {!initialGoals.length && (
-            <div className="w-full h-full flex-row justify-center items-center ">
-              <Layers3 size={32} className="text-4xl" />
-              <span className="text-3xl">Aun no tienes Desafios</span>
-            </div>
-          )}
-          {initialGoals?.map((goal) => (
-            <GoalCard
-              icon={<Medal className="w-8 h-8 text-primary-foreground" />}
-              goalId={goal.id}
-              state={goal.State}
-              title={goal.title}
-              key={goal.title}
-              description={goal.description}
+        {!initialGoals.length ? (
+          <div className="w-full h-full flex-row justify-center items-center ">
+            <Layers3
+              size={32}
+              className="text-4xl text-black dark:text-white"
             />
-          ))}
-        </div>
+            <span className="text-3xl text-black dark:text-white">
+              Aun no tienes Desafios
+            </span>
+          </div>
+        ) : (
+          <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-2  overflow-y-scroll h-[calc(100vh-300px)]">
+            {loading && (
+              <div className="w-full mx-auto flex justify-center">
+                <Loader2 width={50} height={50} className="animate-spin" />
+              </div>
+            )}
+            {initialGoals?.map((goal) => (
+              <GoalCard
+                icon={<Medal className="w-8 h-8 text-primary-foreground" />}
+                goalId={goal.id}
+                state={goal.State}
+                title={goal.title}
+                key={goal.title}
+                description={goal.description}
+              />
+            ))}
+          </div>
+        )}
       </div>
     </>
   );
