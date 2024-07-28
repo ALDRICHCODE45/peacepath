7f35aeed520d909c747a8fc8b3e06b023dc29a95
ALDRICH_CODE45 <nocheblanca92@gmail.com>
2024-07-27 18:35:26 -0600

    modal Trigger added to alert

--- src/components/meditation/Modal-details.tsx
+++ src/components/meditation/Modal-details.tsx
@@ -28,7 +28,7 @@ export function ModalDetails({ details }: Props) {
       <Modal>
         <ModalTrigger className="bg-black dark:bg-white dark:text-black text-white flex justify-center group/modal-btn">
           <span className="group-hover/modal-btn:translate-x-40 text-center transition duration-500 text-sm">
-            Saber Mas.
+            Detalles
           </span>
           <div className="-translate-x-40 group-hover/modal-btn:translate-x-0 flex items-center justify-center absolute inset-0 transition duration-500 text-white z-20">
             🌟
@@ -86,10 +86,7 @@ export function ModalDetails({ details }: Props) {
           </ModalContent>
           <ModalFooter className="gap-4">
             <button className="px-2 py-1 bg-gray-200 text-black dark:bg-black dark:border-black dark:text-white border border-gray-300 rounded-md text-sm w-28">
-              Cancel
-            </button>
-            <button className="bg-black text-white dark:bg-white dark:text-black text-sm px-2 py-1 rounded-md border border-black w-28">
-              Book Now
+              Atras
             </button>
           </ModalFooter>
         </ModalBody>
