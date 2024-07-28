ef4a3c8547bd7c3afde1115bdac97bec4cdeb3b8
ALDRICH_CODE45 <nocheblanca92@gmail.com>
2024-07-27 18:32:19 -0600

    text-sm to mdal trigger

--- src/components/meditation/Modal-details.tsx
+++ src/components/meditation/Modal-details.tsx
@@ -27,7 +27,7 @@ export function ModalDetails({ details }: Props) {
     <div className="  flex items-center justify-center">
       <Modal>
         <ModalTrigger className="bg-black dark:bg-white dark:text-black text-white flex justify-center group/modal-btn">
-          <span className="group-hover/modal-btn:translate-x-40 text-center transition duration-500">
+          <span className="group-hover/modal-btn:translate-x-40 text-center transition duration-500 text-sm">
             Saber Mas.
           </span>
           <div className="-translate-x-40 group-hover/modal-btn:translate-x-0 flex items-center justify-center absolute inset-0 transition duration-500 text-white z-20">
